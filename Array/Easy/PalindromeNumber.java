0fd39252b33f3e531132a699be4cf1ebc1c4af47
SheetalHub-source <sheetalrajput3497@gmail.com>
2025-10-03 20:14:28 +0530

    DSA Day 44: Implemented Palindrome Number

---
+++ Array/Easy/PalindromeNumber.java
@@ -0,0 +1,22 @@
+package Easy;
+
+public class PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x > 0 && x % 10 == 0)) {
+            return false;
+        }
+        int reversedHalf = 0;
+
+        while (reversedHalf < x) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+
+    public static void main(String[] args) {
+        int n = 121;
+        System.out.println(isPalindrome(n));
+    }
+}
