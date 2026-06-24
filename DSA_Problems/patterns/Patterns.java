5a3bcddecb19eb8b99cec0508ef92671c7ae9d2e
SheetalHub-source <sheetalrajput3497@gmail.com>
2026-06-24 11:09:48 +0530

    Implement and learn palindrome letter pyramid pattern

--- DSA_Problems/patterns/Patterns.java
+++ DSA_Problems/patterns/Patterns.java
@@ -152,18 +152,32 @@ public class Patterns {
             character++;
             System.out.println();
         }}
-    private static void printPalindromicLetterPyramid(int n){
-        char character = 'A';
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<i;j++){
-                char letter = (char) (character + j);
+    private static void printPalindromicLetterPyramid(int n) {
+//        char character = 'A';
+//        for(int i=1;i<=n;i++){
+//            for(int j=0;j<i;j++){
+//                char letter = (char) (character + j);
+//                System.out.print(letter);
+//            }
+//            for(int j=i-2;j>=0;j--){
+//                char letter = (char) (character + j);
+//                System.out.print(letter);
+//            }
+//            character++;
+//            System.out.println();
+//        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 0; j--) {
+                char letter = (char) ('A' + j);
                 System.out.print(letter);
             }
-            for(int j=i-2;j>=0;j--){
-                char letter = (char) (character + j);
+            for (int j = 1; j <= i; j++) {
+                char letter = (char) ('A' + j);
                 System.out.print(letter);
             }
-            character++;
             System.out.println();
         }
     }
